aebd148a4adc45cf3bf18d1570d3e805be034b3c
manueo <mail@manueo.com>
2025-03-04 22:24:26 +0100

    added basic testcase file

---
+++ src/TestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Danube\Core;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase
+{
+    
+    protected string $lineBreakInTestConsole = "\n  ";
+
+    protected function linkeBreakInTestConsoleWithFileInfo(): string
+    {
+        return $this->lineBreakInTestConsole . $this->lineBreakInTestConsole . 'The error was triggered in class: ' . get_called_class();
+    }
+
+    protected function assertAttributeInClassExists(string $attribute, string $classNameWithNamespace)
+    {
+        $propertyCheck = property_exists($classNameWithNamespace, $attribute);
+        if (!$propertyCheck) {
+            $this->fail("Attribute: $attribute does not exist in $classNameWithNamespace" . $this->linkeBreakInTestConsoleWithFileInfo());
+        } else {
+            $this->assertTrue($propertyCheck);
+        }
+    }
+
+    protected function assertClassExist(string $classNameWithNamespace)
+    {
+        $classExists = class_exists($classNameWithNamespace);
+        if (!$classExists) {
+            $this->fail($classNameWithNamespace . " does not exist." . $this->linkeBreakInTestConsoleWithFileInfo());
+        } else {
+            $this->assertTrue($classExists);
+        }
+    }
+
+}
+
