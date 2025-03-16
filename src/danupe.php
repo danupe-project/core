1dade635a2ddd8eed6dca6760c67c3d9a15b111f
manueo <mail@manueo.com>
2025-03-16 22:19:20 +0100

    refactored to only helper load

--- src/danupe.php
+++ src/danupe.php
@@ -1,10 +1,6 @@
 <?php
-require dirname(__DIR__, 4). '/vendor/autoload.php';
 require __DIR__.'/helper.php';
-
-use Danube\Core\Classes\Env;
-Env::init();
-
+require dirname(__DIR__, 4). '/vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 $app = AppFactory::create();
