788c8a6d95560036af1c98e051fab3453d5bb971
manueo <mail@manueo.com>
2025-03-16 22:20:01 +0100

    added primary new helper functions

--- src/helper.php
+++ src/helper.php
@@ -8,6 +8,50 @@ if (!function_exists('dd')) {
     }
 }
 
+if (!function_exists('config')) {
+    $configInstance = null;
+
+    function config($key, $default = '')
+    {
+        global $configInstance;
+
+        if ($configInstance === null) {
+            $configInstance = new \Danupe\Core\Classes\Config();
+        }
+
+        return $configInstance->get($key, $default);
+    }
+}
+
+if (!function_exists('env')) {
+    $envInstance = null;
+
+    function env($key, $default = '')
+    {
+        global $envInstance;
+
+        if ($envInstance === null) {
+            $envInstance = new Danupe\Core\Classes\Env();
+        }
+
+        return $envInstance::get($key, $default);
+    }
+}
+
+if (!function_exists('envArray')) {
+
+    function envArray($key, $default = [], $delimiter='')
+    {
+        global $envInstance;
+
+        if ($envInstance === null) {
+            $envInstance = new Danupe\Core\Classes\Env();
+        }
+
+        return $envInstance::getArray($key, $default, $delimiter);
+    }
+}
+
 if (!function_exists('d')) {
     function d($data)
     {
@@ -26,21 +70,23 @@ if (!function_exists('dump')) {
 
         if (php_sapi_name() == 'cli') {
             var_dump($data);
+            echo "Called from: " . $caller['file'] . " on line " . $caller['line'] . "\n\n";
         } else {
-            echo "<pre>";
-            var_dump($data);
+            echo "<div  style='background: #222; color:#00cc00; padding:5px;  font-size:12px; font-family:verdana'>DEBUG:";
+            echo "<pre style='margin:0; font-weight:bold; font-size:14px; font-family:verdana'>";
+            print_r($data);
             echo "</pre>";
 
             echo "\nStack trace:\n";
             $i = 0;
             foreach ($trace as $step) {
                 if ($i < 3) {
-                    echo $step['file'] . " (" . $step['line'] . "): " . (isset($step['function']) ? $step['function'] : 'N/A') . "\n";
+                    echo $step['file'] . " (" . $step['line'] . "): " . (isset($step['function']) ? $step['function'] : 'N/A') . "<br />";
                     $i++;
                 }
             }
+            echo "Called from: " . $caller['file'] . " on line " . $caller['line'] . "<br /><br />";
+            echo "</div>";
         }
-        echo "Called from: " . $caller['file'] . " on line " . $caller['line'] . "\n\n";
-
     }
 }
