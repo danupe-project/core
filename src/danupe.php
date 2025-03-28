4dfe7b13a6906fd5da926084f3b9e127e5ab9d23
manueo <mail@manueo.com>
2025-03-28 14:28:54 +0100

    added session, csrf token and web routes

--- src/danupe.php
+++ src/danupe.php
@@ -2,5 +2,12 @@
 require __DIR__.'/helper.php';
 require dirname(__DIR__, 4). '/vendor/autoload.php';
 
+session_start();
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
 use Slim\Factory\AppFactory;
 $app = AppFactory::create();
+
+include __DIR__.'/routes/web.php';
