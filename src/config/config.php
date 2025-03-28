8c3cfc8b75a32d6164632b4f9961e66a44b7b643
manueo <mail@manueo.com>
2025-03-28 14:32:59 +0100

    added settings

--- src/config/config.php
+++ src/config/config.php
@@ -1,3 +1,10 @@
 <?php
 
-return [];
+return [
+    'prefix' => 'danupe',
+    'meta' => [
+        'title' => 'Danupe | Simple CMS',
+        'description' => 'Danupe is a simple CMS for developers',
+        'keywords' => 'cms, danupe, php, laravel, symfony, wordpress, drupal, joomla'
+    ],
+];
