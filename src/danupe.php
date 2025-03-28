671661ee7a3fef35f461a49bb890ef9c2fcae3a6
manueo <mail@manueo.com>
2025-03-28 14:44:12 +0100

    refactored to danupe()

--- src/danupe.php
+++ src/danupe.php
@@ -2,10 +2,9 @@
 require __DIR__.'/helper.php';
 require dirname(__DIR__, 4). '/vendor/autoload.php';
 
-session_start();
-if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-}
+danupe()->session()->start();
+
+danupe()->session()->setCsrf();
 
 use Slim\Factory\AppFactory;
 $app = AppFactory::create();
