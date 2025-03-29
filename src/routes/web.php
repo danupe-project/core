e586a10dac85696643cb2cbc3cdbdd2cd6af75d7
manueo <mail@manueo.com>
2025-03-29 15:02:51 +0100

    refactored to new routes logic

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,29 +1,22 @@
 <?php
 
-use Danupe\Core\Classes\Config;
+$routes = danupe()->route()->getAll();
+$systemMiddlewares = danupe()->config()->getAllByKey('middlewares', true);
 
-$config = new Config();
-$routes = $config->getRoutes();
+foreach ($routes as $route => $handler) {
+    $method = strtolower($handler['method']);
+    $routeInstance = $app->$method($route, $handler['controller'] . ':' . $handler['action']);
 
-$systemMiddlewares = $config->getAllByKey('middlewares', true);
-
-foreach ($routes as $routeGroup) {
-    foreach ($routeGroup as $method => $routes) {
-        foreach ($routes as $route => $handler) {
-            $routeInstance = $app->$method($route, danupe()->data()->get($handler, 'controller') . ':' . danupe()->data()->get($handler, 'action'));
-            $middlewares = danupe()->data()->get($handler, 'middlewares', []);
-            if (!empty($middlewares)) {
-                foreach ($middlewares as $middlewareClass) {
-
-                    $middlewareClass = danupe()->data()->get($systemMiddlewares, $middlewareClass);
-                    if($middlewareClass){
-                        $routeInstance->add(new $middlewareClass());
-                    }
-
-                    $routeInstance->add(Danupe\Core\Middlewares\CsrfMiddleware::class);
-
-                }
+    $middlewares = $handler['middlewares'] ?? [];
+    if (!empty($middlewares)) {
+        foreach ($middlewares as $middlewareClass) {
+            $middlewareClass = danupe()->data()->get($systemMiddlewares, $middlewareClass);
+            if ($middlewareClass) {
+                $routeInstance->add(new $middlewareClass());
             }
         }
     }
+
+    $routeInstance->add(Danupe\Core\Middlewares\CsrfMiddleware::class);
+
 }
