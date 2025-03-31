59afc69b02b888868d45b00e14cf1c3219f11c3c
manueo <mail@manueo.com>
2025-03-31 20:35:01 +0200

    added render method

--- src/classes/View.php
+++ src/classes/View.php
@@ -11,4 +11,11 @@ class View
         include danupe()->path()->base() . danupe()->path()->plugin($pluginOrProject) . '/src/views/' . $path . '.php';
     }
 
+    public function render(string $pluginOrProject, string $path, $data = [])
+    {
+        ob_start();
+        include danupe()->path()->base() . danupe()->path()->plugin($pluginOrProject) . '/src/views/' . $path . '.php';
+        return ob_get_clean();
+    }
+
 }
